80f3f7c091ee09d5bb0988443f6ba5a9bbff6528
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2022-12-04 11:23:49 +0100

    Use dicts instead of lambda functions in day 2 puzzle

--- solutions/day02.py
+++ solutions/day02.py
@@ -18,52 +18,40 @@ def initialize():
     __P = "B"
     __S = "C"
 
-    __movePoints = {
-        __R: 1,
-        __P: 2,
-        __S: 3
-    }
+    __movePoints = { __R: 1, __P: 2, __S: 3 }
 
     __outcomes = {
-        __R: lambda m: VICTORY if m == __S else (DRAW if m == __R else LOSS),
-        __P: lambda m: VICTORY if m == __R else (DRAW if m == __P else LOSS),
-        __S: lambda m: VICTORY if m == __P else (DRAW if m == __S else LOSS)
+        __R: { __S: VICTORY, __R: DRAW, __P: LOSS },
+        __P: { __R: VICTORY, __P: DRAW, __S: LOSS },
+        __S: { __P: VICTORY, __S: DRAW, __R: LOSS }
     }
 
     with open(inputFile("02"), "r") as f:
         __movesList = list(map(lambda l: l.split(" "), f.read().splitlines()))
 
 def solveFirst():
-    encryptedMove = {
-        "X": __R,
-        "Y": __P,
-        "Z": __S
-    }
+    encryptedMove = { "X": __R, "Y": __P, "Z": __S }
 
     points = 0
     for round in __movesList:
         moveToPlay = encryptedMove[round[1]]
-        roundOutcome = __movePoints[moveToPlay] + __outcomes[moveToPlay](round[0])
+        roundOutcome = __movePoints[moveToPlay] + __outcomes[moveToPlay][round[0]]
 
         points += roundOutcome
 
     return points
 
 def solveSecond():
-    getVictory = lambda m: __P if m == __R else (__S if m == __P else __R)
-    getDraw = lambda m: m
-    getLoss = lambda m: __S if m == __R else (__R if m == __P else __P)
-
     encryptedMove = {
-        "X": lambda m: getLoss(m),
-        "Y": lambda m: getDraw(m),
-        "Z": lambda m: getVictory(m)
+        "X": { __R: __S, __P: __R, __S: __P },
+        "Y": { __R: __R, __P: __P, __S: __S },
+        "Z": { __R: __P, __P: __S, __S: __R }
     }
 
     points = 0
     for round in __movesList:
-        moveToPlay = encryptedMove[round[1]](round[0])
-        roundOutcome = __movePoints[moveToPlay] + __outcomes[moveToPlay](round[0])
+        moveToPlay = encryptedMove[round[1]][round[0]]
+        roundOutcome = __movePoints[moveToPlay] + __outcomes[moveToPlay][round[0]]
 
         points += roundOutcome
 
