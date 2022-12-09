40fc70eb5fe878db3888a5ad1316df34d96da9d0
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2022-12-09 14:01:21 +0100

    Add day 9 second puzzle and optimize day 9 first puzzle

--- solutions/day09.py
+++ solutions/day09.py
@@ -1,67 +1,61 @@
 from solutions import inputFile
 
-def __moveHead(direction):
+def __moveHead(head, direction):
     if direction == "R":
-        __headPos[__X] += 1
+        head[__X] += 1
     elif direction == "L":
-        __headPos[__X] -= 1
+        head[__X] -= 1
     elif direction == "U":
-        __headPos[__Y] -= 1
+        head[__Y] -= 1
     else:
-        __headPos[__Y] += 1
+        head[__Y] += 1
 
-def __tailFollow():
-    xDiff = __headPos[__X] - __tailPos[__X]
-    yDiff = __headPos[__Y] - __tailPos[__Y]
+def __tailFollow(head, tail):
+    xDiff = head[__X] - tail[__X]
+    yDiff = head[__Y] - tail[__Y]
 
-    if abs(xDiff) <= 1 and abs(yDiff) <= 1:
-            # Tail is at least diagonally adjacent to head
-            return
-    else:
+    if abs(xDiff) > 1 or abs(yDiff) > 1:
         # Follow head
         xMove = -1 if xDiff < 0 else (0 if xDiff == 0 else 1)
         yMove = -1 if yDiff < 0 else (0 if yDiff == 0 else 1)
 
-        __tailPos[__X] += xMove
-        __tailPos[__Y] += yMove
+        tail[__X] += xMove
+        tail[__Y] += yMove
 
 def initialize():
-    global __headPos
-    global __tailPos
-    global __tailVisited
-
-    global __moves
-
-    global __DIRECTION
-    global __AMOUNT
-
     global __X
     global __Y
 
-    __DIRECTION = 0
-    __AMOUNT = 1
-    
-    __X = "x"
-    __Y = "y"
+    global __firstTailVisited
+    global __lastTailVisited
 
-    __tailVisited = set()
+    DIRECTION = 0
+    AMOUNT = 1
+    
+    __X = 0
+    __Y = 1
 
     with open(inputFile("09"), "r") as f:
-        __moves = map(lambda l: l.split(" "), f.read().splitlines())
+        moves = map(lambda l: l.split(" "), f.read().splitlines())
     
-    __moves = list(map(lambda m: [m[__DIRECTION], int(m[__AMOUNT])], __moves))
+    moves = list(map(lambda m: [m[DIRECTION], int(m[AMOUNT])], moves))
+    
+    rope = [[0, 0] for _ in range(0, 10)]
 
-    __headPos = { __X: 0, __Y: 0 }
-    __tailPos = { __X: 0, __Y: 0 }
+    __firstTailVisited = set()
+    __lastTailVisited = set()
+
+    for m in moves:
+        for _ in range(0, m[AMOUNT]):
+            __moveHead(rope[0], m[DIRECTION])
+            for knotIndex in range(1, len(rope)):
+                __tailFollow(rope[knotIndex - 1], rope[knotIndex])
+
+            __firstTailVisited.add((rope[1][__X], rope[1][__Y]))
+            __lastTailVisited.add((rope[len(rope) - 1][__X], rope[len(rope) - 1][__Y]))
 
 def solveFirst():
-    for m in __moves:
-        for _ in range(0, m[__AMOUNT]):
-            __moveHead(m[__DIRECTION])
-            __tailFollow()
-            __tailVisited.add((__tailPos[__X], __tailPos[__Y]))
-
-    return len(__tailVisited)
+    return len(__firstTailVisited)
 
 def solveSecond():
-    return 1
+    return len(__lastTailVisited)
