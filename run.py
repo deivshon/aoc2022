759161589ed79b9140b126480ff88d2997cab744
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2022-12-10 01:01:57 +0100

    Centralize solutions list + add testing with GitHub Actions

--- run.py
+++ run.py
@@ -2,37 +2,7 @@ import sys
 
 from time import perf_counter_ns
 
-from solutions import day01
-from solutions import day02
-from solutions import day03
-from solutions import day04
-from solutions import day05
-from solutions import day06
-from solutions import day07
-from solutions import day08
-from solutions import day09
-
-INIT = "init"
-FIRST = "first"
-SECOND = "Second"
-
-buildSolution = lambda init, first, second: {
-    INIT: init,
-    FIRST: first,
-    SECOND: second
-}
-
-solutions = (
-    buildSolution(day01.initialize, day01.solveFirst, day01.solveSecond),
-    buildSolution(day02.initialize, day02.solveFirst, day02.solveSecond),
-    buildSolution(day03.initialize, day03.solveFirst, day03.solveSecond),
-    buildSolution(day04.initialize, day04.solveFirst, day04.solveSecond),
-    buildSolution(day05.initialize, day05.solveFirst, day05.solveSecond),
-    buildSolution(day06.initialize, day06.solveFirst, day06.solveSecond),
-    buildSolution(day07.initialize, day07.solveFirst, day07.solveSecond),
-    buildSolution(day08.initialize, day08.solveFirst, day08.solveSecond),
-    buildSolution(day09.initialize, day09.solveFirst, day09.solveSecond)
-)
+from solutions.list import *
 
 solutionsTimeSum = 0
 
@@ -53,7 +23,7 @@ def timeSolution(label, solution):
     return msElapsed
 
 start = 0
-end = len(solutions)
+end = len(solutionTests)
 
 if len(sys.argv) > 1:
     try:
@@ -63,11 +33,11 @@ if len(sys.argv) > 1:
         print("Error: day argument must be an integer", file = sys.stderr)
         sys.exit(1)
 
-if start >= len(solutions) or start < 0:
+if start >= len(solutionTests) or start < 0:
         print(f"Error: day {start + 1} solution does not exist", file = sys.stderr)
         sys.exit(1)
 
 for i in range(start, end):
-    timeSolution(f"{i + 1}", solutions[i])
+    timeSolution(f"{i + 1}", solutionTests[i])
 
 print(f"\nTotal: {solutionsTimeSum:.5f}ms")
